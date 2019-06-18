ea920a425061da1086c5479f352780e63a2a9e28
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-06-17 22:47:34 -0400

    Add files via upload
    Initial commit for Proj2

---
+++ Proj2/AntigenFrequencyDB.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE fastBlood_BD;
+
+USE fastBlood_BD;
+
+CREATE TABLE antigenFrequency{
+    Antigen VARCHAR (3) NOT NUL,
+    Frequency Whites INT (2) NOT NULL,
+    Frequency Blacks INT (2) NOT NULL,
+};
+
+--  WORKING OFF THE ODD THAT THE ANTIGEN IS NOT PRESENT , USING ANTIGEN NEGATIVE FREQUENCY
+
+
+INSERT INTO antigenFrequency 
+VALUES (D,85,92);
+INSERT INTO antigenFrequency 
+VALUES (C,68,27);
+INSERT INTO antigenFrequency
+VALUES(E,29,22);
+INSERT INTO antigenFrequency
+VALUES(c,80,96);
+INSERT INTO antigenFrequency
+VALUES(e,98,98);
+
+-- ^^^^^^^flip values to negative FOR CALCULATION
+INSERT INTO antigenFrequency
+VALUES(M,21,26);
+INSERT INTO antigenFrequency
+VALUES(N,30,25);
+INSERT INTO antigenFrequency
+VALUES(S,48,68);
+INSERT INTO antigenFrequency
+VALUES(s,48,68);
+INSERT INTO antigenFrequency
+VALUES(K,91,98);
+INSERT INTO antigenFrequency
+VALUES(k,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
+INSERT INTO antigenFrequency
+VALUES(,,);
---
+++ Proj2/README.md
@@ -0,0 +1,26 @@
+#TITLE
+
+##client-side tracking mechanism 
+
+###A simulator that predicts the amount of time to recieve antigen types blood, and simulates their progresss via hospital location map, so that you can test the app independently from any client-side vehicle locator.
+##  Some people in the patient care chain may have limited understanding of the difficulties in finding rare units of blood, particularly in clinically urgent situations. To help understanding and communication between blood providers, transfusion laboratories, clinicians, and patients.
+
+#Systems used
+## -Basic knowledge of Node.js, NPM, and SQL
+
+https://transfusion.com.au/node/727
+
+https://www.jqueryscript.net/loading/Customizable-Radial-Progress-Bar-Plugin-With-jQuery.html
+
+
+https://codelabs.developers.google.com/codelabs/transport-tracker-backend/#3
+
+
+https://trackingjs.com/docs.html#web-components
+
+https://pusher.com/tutorials/live-progress-react
+
+//progress bar
+https://codemyui.com/place-order-progress-bar-animation-steps/
+
+https://www.semanticscholar.org/paper/Advances-in-Blood-Typing.-Quraishy-Sapatnekar/e79aa0a2ab94329060a0bbaec024522c3a9b701f/figure/8
